844dc85baecd1cedbca460046419af0604aec338
ycg <ycg@taipuauto.cn>
2018-05-21 10:57:50 +0800

    feat:static change

--- ask_price/settings/base.py
+++ ask_price/settings/base.py
@@ -135,7 +135,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/manage/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/").replace('\\', '/')
 
 # STATICFILES_DIRS = (
